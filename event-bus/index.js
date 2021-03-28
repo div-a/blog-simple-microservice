3cc41a3c4b284eec806b75f54b5e882bee6a20b3
DESKTOP-3G40DR1\Diva <diva.lonial@gmail.com>
2021-03-28 12:19:51 +0800

    moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const axios = require('axios')
 
 const app = express();
-app.use(bodyParser.json())
+app.use(express.json())
 
 app.post('/events', (req, res) => {
     const event = req.body;
@@ -11,6 +10,7 @@ app.post('/events', (req, res) => {
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
     axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4003/events', event);
 
     res.send({status : 'OK'});
 })
